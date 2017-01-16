19e620c322a67d4ee03f45130d5a337711d7bbe2
tharon-c <design@tharoncarlson.com>
2017-01-15 21:55:19 -0700

    Basic images and providers api

---
+++ server/data.js
@@ -0,0 +1,106 @@
+exports.images = [
+    {
+        id:'1',
+        uid: 'dsd9o9r3ujf',
+        name: "Batman",
+        provider: "1",
+        size: "1"
+    },
+    {
+        id:'2',
+        uid: '343vo9r3ujf',
+        name: "Superman",
+        provider: "1",
+        size: "3"
+    },
+    {
+        id:'3',
+        uid: '64ggfggr3ujf',
+        name: "Wolverine",
+        provider: "1",
+        size: "2"
+    },
+    {
+        id:'4',
+        uid: '54ggr3ujfdgd',
+        name: "Danger Mouse",
+        provider:"2",
+        size: "1"
+    },
+    {
+        id:'5',
+        uid: '54grg4t9r3ujf',
+        name: "Deadpool",
+        provider: "5",
+        size: "3"
+    },
+];
+
+exports.providers = [
+    {
+        id:'1',
+        name: "America",
+        sizes: [
+            {
+                id: "1",
+                name: "A-xsm",
+            },
+            {
+                id: "2",
+                name: "A-sm",
+            },
+            {
+                id: "3",
+                name: "A-m",
+            },
+            {
+                id: "4",
+                name: "A-l",
+            },
+        ],
+    },
+    {
+        id:'2',
+        name: "UK",
+        sizes: [
+            {
+                id: "1",
+                name: "U-xsm",
+            },
+            {
+                id: "2",
+                name: "U-sm",
+            },
+            {
+                id: "3",
+                name: "U-m",
+            },
+            {
+                id: "4",
+                name: "U-l",
+            },
+        ],
+    },
+    {
+        id:'5',
+        name: "Canada",
+        sizes: [
+            {
+                id: "1",
+                name: "C-xsm",
+            },
+            {
+                id: "2",
+                name: "C-sm",
+            },
+            {
+                id: "3",
+                name: "C-m",
+            },
+            {
+                id: "4",
+                name: "C-l",
+            },
+        ],
+    },
+];
--- server/server.js
+++ server/server.js
@@ -3,71 +3,146 @@ var bodyParser = require('body-parser');
 var app = express();
 var _ = require('lodash');
 var morgan = require('morgan');
+var genUid = require('../genUid');
+var data = require('./data');
 
-var lions = [];
 var id = 0;
 
+var copy = arr => arr.map(a => Object.assign({}, a));
+var images = copy(data.images);
+var providers = copy(data.providers);
+
 var updateId = function(req, res, next) {
-  if (!req.body.id) {
-    id++;
-    req.body.id = id + '';
-  }
-  next();
+    if (!req.body.id) {
+        req.body.id = genUid.uid();
+    }
+    next();
+};
+function findWithAttr(array, attr, value) {
+    for(var i = 0; i < array.length; i += 1) {
+        if(array[i][attr] === value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
 };
 
 app.use(morgan('dev'))
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(allowCrossDomain);
 
 app.use(function(err, req, res, next) {
-  if (err) {
-    res.status(500).send(error);
-  }
+    if (err) {
+        res.status(500).send(error);
+    }
 });
 
-app.param('id', function(req, res, next, id) {
-  var lion = _.find(lions, {id: id});
-
-  if (lion) {
-    req.lion = lion;
-    next();
-  } else {
-    res.send();
-  }
+//===========
+// Control
+// ==========
+//
+app.get('/reset', function(req, res) {
+    images = copy(data.images);
+    providers = copy(data.providers);
+    res.json({images:images, providers:providers});
 });
 
-app.get('/lions', function(req, res){
-  res.json(lions);
+//===========
+// Images 
+// ==========
+//
+app.get('/images', function(req, res){
+    setTimeout(
+        () => { res.json(images) },
+    500);
 });
 
-app.get('/lions/:id', function(req, res){
-  var lion = req.lion;
-  res.json(lion || {});
+app.get('/images/:id', function(req, res){
+    var id = req.params.id;
+    var image = _.find(images, {id: id});
+    res.json(image || {});
 });
 
-app.post('/lions', updateId, function(req, res) {
-  var lion = req.body;
-
-  lions.push(lion);
-
-  res.json(lion);
+app.post('/images', updateId, function(req, res) {
+    var image = req.body;
+    images.push(image);
+    setTimeout(
+        () => { res.json(image) },
+    1000);
 });
 
 
-app.put('/lions/:id', function(req, res) {
-  var update = req.body;
-  if (update.id) {
-    delete update.id
-  }
+app.put('/images/:id', function(req, res) {
+    var update = req.body;
+    if (update.id) {
+        delete update.id
+    }
 
-  var lion = _.findIndex(lions, {id: req.params.id});
-  if (!lions[lion]) {
-    res.send();
-  } else {
-    var updatedLion = _.assign(lions[lion], update);
-    res.json(updatedLion);
-  }
+    var image = _.findIndex(images, {id: req.params.id});
+    if (!images[image]) {
+        res.send();
+    } else {
+        var updatedimage = _.assign(images[image], update);
+        res.json(updatedimage);
+    }
+});
+
+app.delete('/images', function(req, res) {
+    var image = req.body;
+    var index = findWithAttr(images, "id", image.id);
+    if ( index === -1 ) { 
+        res.send() 
+    } else {
+        images.splice(index, 1);
+        res.json(image);
+    }
+});
+
+//===========
+// providers 
+// ==========
+//
+app.get('/providers', function(req, res){
+    setTimeout(
+        () => { res.json(providers) },
+    600);
+});
+
+app.get('/providers/:id', function(req, res){
+    var id = req.params.id;
+    var provider = _.find(providers, {id: id});
+    res.json(provider || {});
+});
+
+app.post('/providers', updateId, function(req, res) {
+    var provider = req.body;
+    providers.push(provider);
+    res.json(provider);
+});
+
+app.put('/providers/:id', function(req, res) {
+    var update = req.body;
+    if (update.id) {
+        delete update.id
+    }
+    var provider = _.findIndex(providers, {id: req.params.id});
+    if (!providers[provider]) {
+        res.send();
+    } else {
+        var updatedProvider = _.assign(providers[provider], update);
+        providers[provider] = updatedProvider;
+
+        res.json(updatedProvider);
+    }
 });
 
 app.listen(3000);
